92eefe5303f3864fc8c7c1314855d22edb56b72d
hossam <hossamj2004@yahoo.com>
2024-08-29 10:21:28 +0300

    fix

--- src/Providers/MenuServiceProvider.php
+++ src/Providers/MenuServiceProvider.php
@@ -10,7 +10,10 @@ class MenuServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->publishes([
+            __DIR__.'/../database/migrations/' => database_path('migrations'),
+        ], 'migrations');
+
         if (Schema::hasTable('menu_items')) {
             $this->loadMenuItems();
         }
